93b04588193d898cc1bbba67e9eaca5b6642cbc0
burak-sisci <buraksisci32@gmail.com>
2026-04-05 10:24:34 +0300

    restart

--- frontend/src/api/listApi.js
+++
@@ -1,6 +0,0 @@
-import API from './axiosInstance';
-
-export const getLists = () => API.get('/lists');
-export const createList = (data) => API.post('/lists', data);
-export const addItemToList = (id, data) => API.put(`/lists/${id}/items`, data);
-export const deleteList = (id) => API.delete(`/lists/${id}`);
